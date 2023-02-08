9e741bc79641970d7325c7e934c5906bad4b90b8
Marinho <jeffersonmarinho97@gmail.com>
2023-02-07 22:21:32 -0300

    feat:creation of models Product and Store

---
+++ app/Store.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    //exemplo de declaração nome da tabela 
+    // protected $table = 'lojas'; 
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/model', function() {
+    // $products = \App\Product::all(); // pegar todos dados da tabela products
+    //all -> busca todos dados tabela
+    //find -> busca uma id em especifico na tabela
+    //where -> clausula para busca ::where(coluna, valor)-> get()\\ retornar todos ||first()\\ retornar primeiro valor
+    //paginate(quantidade por pagina)
+    // Mass Assignment
+    // $user =\App\User::create([
+    //     'name' => 'Jefferson Marinho',
+    //     'email' => 'jeffersonmarinho@gmail.com',
+    //     'password' => bcrypt('12345678')
+    // ]);
+
+    //Mass Update
+    $user = \App\User::find(42);
+    $user->update([
+        'name' => 'Atualizando com Mass Update'
+    ]);
+    dd($user);
+
+    // return \App\User::paginate(5);
+});
