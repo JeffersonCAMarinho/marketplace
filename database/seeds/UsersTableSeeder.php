3b718786a3c5ba3bda41f588535e72011c6f344f
Marinho <jeffersonmarinho97@gmail.com>
2023-02-13 21:48:25 -0300

    feat: create Store Seed and create relationship between User seed and Store seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,8 @@ class UsersTableSeeder extends Seeder
         //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         //     'remember_token' => Str::random(10),
         // ]);
-        factory(\App\User::class, 40)->create();
+        factory(\App\User::class, 40)->create()->each(function($user){
+            $user->store()->save(factory(\App\Store::class)->make());
+        });
     }
 }
