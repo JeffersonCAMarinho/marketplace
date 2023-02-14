564ceebffc2d9a7dfa24f6ca31d6ab1e0efbda3e
Marinho <jeffersonmarinho97@gmail.com>
2023-02-13 21:44:36 -0300

    feat: adding relationship between models

--- app/Store.php
+++ app/Store.php
@@ -8,6 +8,15 @@ class Store extends Model
 {
     //exemplo de declaração nome da tabela 
     // protected $table = 'lojas'; 
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
 
-    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
